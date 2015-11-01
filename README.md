5e4ee1c03001cb48dc5da7680668e4cfbfe21ffe
SteamingMutt <SteamingMutt@users.noreply.github.com>
2015-11-01 22:39:59 +0100

    Initial Commit
    Removed a few commands to make it operational

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# DougleyBot
-A Discord bot, modified version of the bot from https://github.com/chalda/DiscordBot/
+# DiscordBot
+A chat bot for discord app based off <a href="https://github.com/hydrabolt/discord.js/">discord.js</a>
+
+# Features:
+- !gif query = returns a gif example !gif cute cats doing stuff
+- !game nameofgame => asks the room if anyone wants to play games! "cs" and "hots" are specially defined
+- !image query => returns an image (careful, no adult filter)
+- !youtube query=> returns a youtube link
+- !wiki query=> returns the summary of the first search result on Wikipedia
+- !say text => echos text
+- !pullanddeploy => pulls changes from your (or this) repo and restarts node. does <strong>not</strong> work for windows!
+- !meme memetype "text1" "text2" => returns a meme image. notice the quotes around text, they are vitally important
+- !help => prints all commands with usage and description;
+- !version => last deployed commit
+- !servers => returns servers this bot is in
+- !channels => returns channels this bot is in
+- !idle => sets bot status to idle
+- !online => sets bot status to online
+- !ping => responds to user with pong!
+- !join-server => bot will join the requested server
+- !create => create a channel
+- !delete => deletes a channel
+- @botname => responds when @mentioned
+
+## RSS:
+    you can create an rss.json file adding rss feeds as commands. See rss.json.example for details
+
+# ToDo:
+
+- Permissions!
+- Link history
+- refactor discord_bot.js. split the msg string, look for command, process instead of a big if block luls (and the whole project. all of this is just quick and dirty)
+- make it a module so you can npm install
+- better plugin layout, allow for easy plugin drop ins, turn on/turn off
+- "pugbomb" returns x number of pug images (pug are an example) corgibomb etc
+- automatically pull in meme codes and do a fuzzy search on meme type
+- voice intergration and DJ features!
+- All the things!
+
+# Instructions
+
+requires node (probably 0.12)
+
+pull repo
+
+add auth.json: email/password, youtube API key, username/password for imgflip (example provided)
+
+npm install
+
+node discord_bot.js
+
+# For non-technical users:
+
+1) google and download "node.js msi download" 
+
+2) go through the installer, this tutorial might help http://blog.teamtreehouse.com/install-node-js-npm-windows
+
+3) once installed download this project as a zip from github
+
+4) unpack and navigate to the project on your PC
+
+5) create a file "auth.json" exactly like the auth.json.example that is provided. Replace the information in there with your own.  You can even use your own credentials, itll respond from your name. Or create a new account and add it to your server. The new file should be placed right along side everythign else.
+
+6) Open cmd prompt (hit windows + q and type in cmd). Test that node works, npm -v and node -v should return something.
+
+7) Navigate to wherever you extracted the project with cd. "cd C:\Users\Alex\Downloads\DiscordBot\"
+
+8) Download requirements with "npm install" 
+	- it seems a lot of people have problems with this step, please follow this guide: http://stackoverflow.com/questions/21365714/nodejs-error-installing-with-npm
+
+9) Run the bot with "node discord_bot.js"
---
+++ discord_bot.js
@@ -0,0 +1,436 @@
+/*
+	this bot is a ping pong bot, and every time a message
+	beginning with "ping" is sent, it will reply with
+	"pong".
+*/
+
+var Discord = require("discord.js");
+
+var yt = require("./youtube_plugin");
+var youtube_plugin = new yt();
+
+var gi = require("./google_image_plugin");
+var google_image_plugin = new gi();
+
+// Get the email and password
+var AuthDetails = require("./auth.json");
+var qs = require("querystring");
+
+var htmlToText = require('html-to-text');
+
+var config = {
+    "api_key": "dc6zaTOxFJmzC",
+    "rating": "r",
+    "url": "http://api.giphy.com/v1/gifs/search",
+    "permission": ["NORMAL"]
+};
+
+
+//https://api.imgflip.com/popular_meme_ids
+var meme = {
+	"brace": 61546,
+	"mostinteresting": 61532,
+	"fry": 61520,
+	"onedoesnot": 61579,
+	"yuno": 61527,
+	"success": 61544,
+	"allthethings": 61533,
+	"doge": 8072285,
+	"drevil": 40945639,
+	"skeptical": 101711,
+	"notime": 442575,
+	"yodawg": 101716
+};
+
+var game_abbreviations = {
+    "cs": "Counter-Strike",
+    "hon": "Heroes of Newerth",
+    "hots": "Heroes of the Storm",
+    "sc2": "Starcraft II",
+    "gta": "Grand Theft Auto"
+};
+
+var commands = {
+	"gif": {
+		usage: "<image tags>",
+        description: "returns a random gif matching the tags passed",
+		process: function(bot, msg, suffix) {
+		    var tags = suffix.split(" ");
+		    get_gif(tags, function(id) {
+			if (typeof id !== "undefined") {
+			    bot.sendMessage(msg.channel, "http://media.giphy.com/media/" + id + "/giphy.gif [Tags: " + (tags ? tags : "Random GIF") + "]");
+			}
+			else {
+			    bot.sendMessage(msg.channel, "Invalid tags, try something different. [Tags: " + (tags ? tags : "Random GIF") + "]");
+			}
+		    });
+		}
+	},
+    "ping": {
+        description: "responds pong, useful for checking if bot is alive",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, msg.sender+" pong!");
+            if(suffix){
+                bot.sendMessage(msg.channel, "note that !ping takes no arguments!");
+            }
+        }
+    },
+    "game": {
+        usage: "<name of game>",
+        description: "pings channel asking if anyone wants to play",
+        process: function(bot,msg,suffix){
+            var game = game_abbreviations[suffix];
+            if(!game) {
+                game = suffix;
+            }
+            bot.sendMessage(msg.channel, "@everyone Anyone up for " + game + "?");
+            console.log("sent game invites for " + game);
+        }
+    },
+    "servers": {
+        description: "lists servers bot is connected to",
+        process: function(bot,msg){bot.sendMessage(msg.channel,bot.servers);}
+    },
+    "channels": {
+        description: "lists channels bot is connected to",
+        process: function(bot,msg) { bot.sendMessage(msg.channel,bot.channels);}
+    },
+    "myid": {
+        description: "returns the user id of the sender",
+        process: function(bot,msg){bot.sendMessage(msg.channel,msg.author.id);}
+    },
+    "idle": {
+        description: "sets bot status to idle",
+        process: function(bot,msg){ bot.setStatusIdle();}
+    },
+    "online": {
+        description: "sets bot status to online",
+        process: function(bot,msg){ bot.setStatusOnline();}
+    },
+    "youtube": {
+        usage: "<video tags>",
+        description: "gets youtube video matching tags",
+        process: function(bot,msg,suffix){
+            youtube_plugin.respond(suffix,msg.channel,bot);
+        }
+    },
+    "say": {
+        usage: "<message>",
+        description: "bot says message",
+        process: function(bot,msg,suffix){ bot.sendMessage(msg.channel,suffix,true);}
+    },
+    "image": {
+        usage: "<image tags>",
+        description: "gets image matching tags from google",
+        process: function(bot,msg,suffix){ google_image_plugin.respond(suffix,msg.channel,bot);}
+    },
+    "pullanddeploy": {
+        description: "bot will perform a git pull master and restart with the new code",
+        process: function(bot,msg,suffix) {
+            bot.sendMessage(msg.channel,"fetching updates...",function(error,sentMsg){
+                console.log("updating...");
+	            var spawn = require('child_process').spawn;
+                var log = function(err,stdout,stderr){
+                    if(stdout){console.log(stdout);}
+                    if(stderr){console.log(stderr);}
+                };
+                var fetch = spawn('git', ['fetch']);
+                fetch.stdout.on('data',function(data){
+                    console.log(data.toString());
+                });
+                fetch.on("close",function(code){
+                    var reset = spawn('git', ['reset','--hard','origin/master']);
+                    reset.stdout.on('data',function(data){
+                        console.log(data.toString());
+                    });
+                    reset.on("close",function(code){
+                        var npm = spawn('npm', ['install']);
+                        npm.stdout.on('data',function(data){
+                            console.log(data.toString());
+                        });
+                        npm.on("close",function(code){
+                            console.log("goodbye");
+                            bot.sendMessage(msg.channel,"brb!",function(){
+                                bot.logout(function(){
+                                    process.exit();
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+        }
+    },
+    "meme": {
+        usage: 'meme "top text" "bottom text"',
+        process: function(bot,msg,suffix) {
+            var tags = msg.content.split('"');
+            var memetype = tags[0].split(" ")[1];
+            //bot.sendMessage(msg.channel,tags);
+            var Imgflipper = require("imgflipper");
+            var imgflipper = new Imgflipper(AuthDetails.imgflip_username, AuthDetails.imgflip_password);
+            imgflipper.generateMeme(meme[memetype], tags[1]?tags[1]:"", tags[3]?tags[3]:"", function(err, image){
+                //console.log(arguments);
+                bot.sendMessage(msg.channel,image);
+            });
+        }
+    },
+    "memehelp": { //TODO: this should be handled by !help
+        description: "returns available memes for !meme",
+        process: function(bot,msg) {
+            var str = "Currently available memes:\n"
+            for (var m in meme){
+                str += m + "\n"
+            }
+            bot.sendMessage(msg.channel,str);
+        }
+    },
+    "log": {
+        usage: "<log message>",
+        description: "logs message to bot console",
+        process: function(bot,msg,suffix){console.log(msg.content);}
+    },
+    "wiki": {
+        usage: "<search terms>",
+        description: "returns the summary of the first matching search result from Wikipedia",
+        process: function(bot,msg,suffix) {
+            var query = suffix;
+            if(!query) {
+                bot.sendMessage(msg.channel,"usage: !wiki search terms");
+                return;
+            }
+            var Wiki = require('wikijs');
+            new Wiki().search(query,1).then(function(data) {
+                new Wiki().page(data.results[0]).then(function(page) {
+                    page.summary().then(function(summary) {
+                        var sumText = summary.toString().split('\n');
+                        var continuation = function() {
+                            var paragraph = sumText.shift();
+                            if(paragraph){
+                                bot.sendMessage(msg.channel,paragraph,continuation);
+                            }
+                        };
+                        continuation();
+                    });
+                });
+            },function(err){
+                bot.sendMessage(msg.channel,err);
+            });
+        }
+    },
+    "join-server": {
+        usage: "<invite>",
+        description: "joins the server it's invited to",
+        process: function(bot,msg,suffix) {
+            console.log(bot.joinServer(suffix,function(error,server) {
+                console.log("callback: " + arguments);
+                if(error){
+                    bot.sendMessage(msg.channel,"failed to join: " + error);
+                } else {
+                    console.log("Joined server " + server);
+                    bot.sendMessage(msg.channel,"Successfully joined " + server);
+                }
+            }));
+        }
+    },
+    "stock": {
+        usage: "<stock to fetch>",
+        process: function(bot,msg,suffix) {
+            var yahooFinance = require('yahoo-finance');
+            yahooFinance.snapshot({
+              symbol: suffix,
+              fields: ['s', 'n', 'd1', 'l1', 'y', 'r'],
+            }, function (error, snapshot) {
+                if(error){
+                    bot.sendMessage(msg.channel,"couldn't get stock: " + error);
+                } else {
+                    //bot.sendMessage(msg.channel,JSON.stringify(snapshot));
+                    bot.sendMessage(msg.channel,snapshot.name
+                        + "\nprice: $" + snapshot.lastTradePriceOnly);
+                }  
+            });
+        }
+    },
+    "rss": {
+        description: "lists available rss feeds",
+        process: function(bot,msg,suffix) {
+            /*var args = suffix.split(" ");
+            var count = args.shift();
+            var url = args.join(" ");
+            rssfeed(bot,msg,url,count,full);*/
+            bot.sendMessage(msg.channel,"Available feeds:", function(){
+                for(var c in rssFeeds){
+                    bot.sendMessage(msg.channel,c + ": " + rssFeeds[c].url);
+                }
+            });
+        }
+    },
+    "reddit": {
+        usage: "[subreddit]",
+        description: "Returns the top post on reddit. Can optionally pass a subreddit to get the top psot there instead",
+        process: function(bot,msg,suffix) {
+            var path = "/.rss"
+            if(suffix){
+                path = "/r/"+suffix+path;
+            }
+            rssfeed(bot,msg,"https://www.reddit.com"+path,1,false);
+        }
+    }
+};
+try{
+var rssFeeds = require("./rss.json");
+function loadFeeds(){
+    for(var cmd in rssFeeds){
+        commands[cmd] = {
+            usage: "[count]",
+            description: rssFeeds[cmd].description,
+            url: rssFeeds[cmd].url,
+            process: function(bot,msg,suffix){
+                var count = 1;
+                if(suffix != null && suffix != "" && !isNaN(suffix)){
+                    count = suffix;
+                }
+                rssfeed(bot,msg,this.url,count,false);
+            }
+        };
+    }
+}
+} catch(e) {
+    console.log("Couldn't load rss.json. See rss.json.example if you want rss feed commands. error: " + e);
+}
+
+function rssfeed(bot,msg,url,count,full){
+    var FeedParser = require('feedparser');
+    var feedparser = new FeedParser();
+    var request = require('request');
+    request(url).pipe(feedparser);
+    feedparser.on('error', function(error){
+        bot.sendMessage(msg.channel,"failed reading feed: " + error);
+    });
+    var shown = 0;
+    feedparser.on('readable',function() {
+        var stream = this;
+        shown += 1
+        if(shown > count){
+            return;
+        }
+        var item = stream.read();
+        bot.sendMessage(msg.channel,item.title + " - " + item.link, function() {
+            if(full === true){
+                var text = htmlToText.fromString(item.description,{
+                    wordwrap:false,
+                    ignoreHref:true
+                });
+                bot.sendMessage(msg.channel,text);
+            }
+        });
+        stream.alreadyRead = true;
+    });
+}
+
+
+var bot = new Discord.Client();
+
+bot.on("ready", function () {
+    loadFeeds();
+	console.log("Ready to begin! Serving in " + bot.channels.length + " channels");
+});
+
+bot.on("disconnected", function () {
+
+	console.log("Disconnected!");
+	process.exit(1); //exit node.js with an error
+	
+});
+
+bot.on("message", function (msg) {
+	//check if message is a command
+	if(msg.author.id != bot.user.id && (msg.content[0] === '!' || msg.content.indexOf(bot.user.mention()) == 0)){
+        console.log("treating " + msg.content + " from " + msg.author + " as command");
+		var cmdTxt = msg.content.split(" ")[0].substring(1);
+        var suffix = msg.content.substring(cmdTxt.length+2);//add one for the ! and one for the space
+        if(msg.content.indexOf(bot.user.mention()) == 0){
+            cmdTxt = msg.content.split(" ")[1];
+            suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+2);
+        }
+		var cmd = commands[cmdTxt];
+        if(cmdTxt === "help"){
+            //help is special since it iterates over the other commands
+            for(var cmd in commands) {
+                var info = "!" + cmd;
+                var usage = commands[cmd].usage;
+                if(usage){
+                    info += " " + usage;
+                }
+                var description = commands[cmd].description;
+                if(description){
+                    info += "\n\t" + description;
+                }
+                bot.sendMessage(msg.channel,info);
+            }
+        }
+		else if(cmd) {
+            cmd.process(bot,msg,suffix);
+		} else {
+			bot.sendMessage(msg.channel, "Invalid command " + cmdTxt);
+		}
+	} else {
+		//message isn't a command or is from us
+        //drop our own messages to prevent feedback loops
+        if(msg.author == bot.user){
+            return;
+        }
+        
+        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+                bot.sendMessage(msg.channel,msg.author + ", you called?");
+        }
+    }
+});
+ 
+
+//Log user status changes
+bot.on("presence", function(data) {
+	//if(status === "online"){
+	//console.log("presence update");
+	console.log(data.user+" went "+data.status);
+	//}
+});
+
+function get_gif(tags, func) {
+        //limit=1 will only return 1 gif
+        var params = {
+            "api_key": config.api_key,
+            "rating": config.rating,
+            "format": "json",
+            "limit": 1
+        };
+        var query = qs.stringify(params);
+
+        if (tags !== null) {
+            query += "&q=" + tags.join('+')
+        }
+
+        //wouldnt see request lib if defined at the top for some reason:\
+        var request = require("request");
+        //console.log(query)
+
+        request(config.url + "?" + query, function (error, response, body) {
+            //console.log(arguments)
+            if (error || response.statusCode !== 200) {
+                console.error("giphy: Got error: " + body);
+                console.log(error);
+                //console.log(response)
+            }
+            else {
+                var responseObj = JSON.parse(body)
+                console.log(responseObj.data[0])
+                if(responseObj.data.length){
+                    func(responseObj.data[0].id);
+                } else {
+                    func(undefined);
+                }
+            }
+        }.bind(this));
+    }
+
+bot.login(AuthDetails.email, AuthDetails.password);
