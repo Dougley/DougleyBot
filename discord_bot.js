45e33502b2b489c82845f67a56936f82cc118de0
JLBP <kingswoodjack@gmail.com>
2015-11-04 01:54:00 +0000

    Added Commands, Slimmed Down
    Added additional commands from NekoBot
    (https://github.com/TehSeph/Neko.js)
    
    Removed The Following Scripts/Modules:
    - Google Image Search
    -- This is due to it been open to abuse
    - Meme Generator (While Good, does not provide a need in my community)

--- discord_bot.js
+++ discord_bot.js
@@ -3,21 +3,17 @@
 	beginning with "ping" is sent, it will reply with
 	"pong".
 */
-
+// Discord Unoffical API
 var Discord = require("discord.js");
-
+// YouTube Data API
 var yt = require("./youtube_plugin");
 var youtube_plugin = new yt();
-
-var gi = require("./google_image_plugin");
-var google_image_plugin = new gi();
-
 // Get the email and password
 var AuthDetails = require("./auth.json");
 var qs = require("querystring");
 
 var htmlToText = require('html-to-text');
-
+// !GIF Support
 var config = {
     "api_key": "dc6zaTOxFJmzC",
     "rating": "r",
@@ -25,41 +21,202 @@ var config = {
     "permission": ["NORMAL"]
 };
 
-
-//https://api.imgflip.com/popular_meme_ids
-var meme = {
-	"brace": 61546,
-	"mostinteresting": 61532,
-	"fry": 61520,
-	"onedoesnot": 61579,
-	"yuno": 61527,
-	"success": 61544,
-	"allthethings": 61533,
-	"doge": 8072285,
-	"drevil": 40945639,
-	"skeptical": 101711,
-	"notime": 442575,
-	"yodawg": 101716,
-	"ermahgerd": 101462,
-	"hipsterariel": 86601,
-	"imagination": 163573,
-	"grumpycat": 405658,
-	"morpheus": 100947
-};
-
-var game_abbreviations = {
-    "cs": "Counter-Strike",
-    "hon": "Heroes of Newerth",
-    "hots": "Heroes of the Storm",
-    "sc2": "Starcraft II",
-	"warf": "Warframe",
-	"gtao": "Grand Theft Auto: Online",
-	"gta": "Grand Theft Auto",
-	"lol": "League of Legends",
-	"wow": "World of Warcraft"
-};
-
 var commands = {
+		//Repond Back Commands
+	    "pet": {
+        usage: "<pets>",
+        description: "Everyone loves being pet, right!?! Pets each *@user*. Leave emtpy (or mention me too) to pet me!",
+        process: function(bot, message, params, errorCallback) {
+
+		// build an array to store pets
+		var pets = [];
+
+		// if everyone is mentioned, skip all other mentions. if nobody is mentioned, nekobot just purrs
+		// TODO: message.everyo	neMentioned is broken so for now we're using indexOf()
+		if (message.mentions.length === 0 || params.indexOf("@everyone") !== -1) {
+			if (params.indexOf("@everyone") !== -1) { pets.push(message.author + " pets @everyone "); }
+			bot.sendMessage(message, pets + "*purrs*").catch(errorCallback);
+			return;
+		}
+
+		// otherwise, cycle mentions and add each user to pets
+		for (index in message.mentions) {
+			var user = message.mentions[index];
+			pets.push(user);
+		}
+
+		// if nekobot is on the list, purr
+		if (message.isMentioned(bot.user)) { pets.push("*purrs*"); }
+
+		// send message
+		bot.sendMessage(message, message.author + " pets " + pets.join(" ")).catch(errorCallback);
+	}
+},
+
+"dank": {
+        usage: "<gives dank memes>",
+        description: "Everyone loves dank memes, right!?!",
+        process: function(bot, message, params, errorCallback) {
+
+		// build an array to store dank
+		var dank = [];
+
+		// if everyone is mentioned, skip all other mentions. if nobody is mentioned, nekobot just purrs
+		// TODO: message.everyo	neMentioned is broken so for now we're using indexOf()
+		if (message.mentions.length === 0 || params.indexOf("@everyone") !== -1) {
+			if (params.indexOf("@everyone") !== -1) { dank.push(message.author + " pets @everyone "); }
+			bot.sendMessage(message, dank + "**All These Dank Memes** http://cdn.meme.am/instances/500x/56642282.jpg").catch(errorCallback);
+			return;
+		}
+
+		// otherwise, cycle mentions and add each user to dank
+		for (index in message.mentions) {
+			var user = message.mentions[index];
+			dank.push(user);
+		}
+
+		// if nekobot is on the list, purr
+		if (message.isMentioned(bot.user)) { dank.push("*Dank*"); }
+
+		// send message
+		bot.sendMessage(message, message.author + " gives " + dank.join(" ") + " Dank Memes").catch(errorCallback);
+	}
+},
+
+"shoot": {
+        usage: "<shoots>",
+        description: "Everyone loves being shot, right!?! shoots each *@user*. Leave emtpy (or mention me too) to shoot me!",
+        process: function(bot, message, params, errorCallback) {
+
+		// build an array to store shoot
+		var shoots = [];
+
+		// if everyone is mentioned, skip all other mentions. if nobody is mentioned, nekobot just purrs
+		// TODO: message.everyo	neMentioned is broken so for now we're using indexOf()
+		if (message.mentions.length === 0 || params.indexOf("@everyone") !== -1) {
+			if (params.indexOf("@everyone") !== -1) { shoots.push(message.author + " pets @everyone "); }
+			bot.sendMessage(message, pets + "*splat*").catch(errorCallback);
+			return;
+		}
+
+		// otherwise, cycle mentions and add each user to shoot
+		for (index in message.mentions) {
+			var user = message.mentions[index];
+			shoots.push(user);
+		}
+
+		// if nekobot is on the list, purr
+		if (message.isMentioned(bot.user)) { shoots.push("*splat*"); }
+
+		// send message
+		bot.sendMessage(message, message.author + " shoots " + shoots.join(" ")).catch(errorCallback);
+	}
+},
+
+"chill": {
+        usage: "<passthe>",
+        description: "Blaze it 420",
+        process: function(bot, message, params, errorCallback) {
+
+		// build an array to store shoot
+		var chills = [];
+
+		// if everyone is mentioned, skip all other mentions. if nobody is mentioned, nekobot just purrs
+		// TODO: message.everyo	neMentioned is broken so for now we're using indexOf()
+		if (message.mentions.length === 0 || params.indexOf("@everyone") !== -1) {
+			if (params.indexOf("@everyone") !== -1) { chills.push(message.author + " pets @everyone "); }
+			bot.sendMessage(message, pets + "*Kek*").catch(errorCallback);
+			return;
+		}
+
+		// otherwise, cycle mentions and add each user to chills
+		for (index in message.mentions) {
+			var user = message.mentions[index];
+			chills.push(user);
+		}
+
+		// if nekobot is on the list, purr
+		if (message.isMentioned(bot.user)) { chills.push("*https://www.youtube.com/watch?v=SjBBDJ5OiT0*"); }
+
+		// send message
+		bot.sendMessage(message, message.author + " Passes the bong to " + chills.join(" ")).catch(errorCallback);
+	}
+},
+
+	//Twitch Streamers Links
+    "timmac": {
+        description: "Link To Timmac Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://twitch.tv/timmac");
+        }
+    },
+    "mrmoon": {
+        description: "Link To Timmac Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://twitch.tv/mrmoonshouse");
+        }
+    },
+    "ming": {
+        description: "Link To Timmac Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://www.twitch.tv/mrbong011");
+        }
+    },
+    "honyolo": {
+        description: "Link To Hon Yolo Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel,"http://twitch.tv/honyolo");
+        }
+    },
+    "floppy": {
+        description: "Link To Floppy Pancakes Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://twitch.tv/floppypancakes");
+        }
+    },
+    "monty": {
+        description: "Link To Monty Brython Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://www.twitch.tv/montybrython");
+        }
+    },
+    "shroomz": {
+        description: "Link To Shroomz Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://www.twitch.tv/shroomztv");
+        }
+    },
+    "jounie": {
+        description: "Link To Jounie's Stream",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://www.twitch.tv/ravencatt");
+        }
+    },
+	//Standard Commands
+    "hype": {
+        description: "timmacHYPE Animated",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://i.imgur.com/20CO8KA.gif");
+        }
+    },	
+    "bd": {
+        description: "information about BetterDiscord",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " Get Twitch Emotes on Discord! Check the **#FAQ** for more info!");
+        }
+    },	
+    "steam": {
+        description: "Steam Community Group",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " http://steamcommunity.com/groups/timmac");
+        }
+    },
+    "johncena": {
+        description: "AND HIS NAME IS",
+        process: function(bot, msg, suffix) {
+            bot.sendMessage(msg.channel, " **AND HIS NAME IS** https://www.youtube.com/watch?v=4k1xY7v8dDQ");
+        }
+    },	
 	"gif": {
 		usage: "<image tags>",
         description: "returns a random gif matching the tags passed",
@@ -92,29 +249,9 @@ var commands = {
             if(!game) {
                 game = suffix;
             }
-            bot.sendMessage(msg.channel, "@everyone Anyone up for " + game + "?");
+            bot.sendMessage(msg.channel, "Anyone up for " + game + "?");
             console.log("sent game invites for " + game);
         }
-    },
-    "servers": {
-        description: "lists servers bot is connected to",
-        process: function(bot,msg){bot.sendMessage(msg.channel,bot.servers);}
-    },
-    "channels": {
-        description: "lists channels bot is connected to",
-        process: function(bot,msg) { bot.sendMessage(msg.channel,bot.channels);}
-    },
-    "myid": {
-        description: "returns the user id of the sender",
-        process: function(bot,msg){bot.sendMessage(msg.channel,msg.author.id);}
-    },
-    "idle": {
-        description: "sets bot status to idle",
-        process: function(bot,msg){ bot.setStatusIdle();}
-    },
-    "online": {
-        description: "sets bot status to online",
-        process: function(bot,msg){ bot.setStatusOnline();}
     },
     "youtube": {
         usage: "<video tags>",
@@ -122,157 +259,11 @@ var commands = {
         process: function(bot,msg,suffix){
             youtube_plugin.respond(suffix,msg.channel,bot);
         }
-    },
-    "say": {
-        usage: "<message>",
-        description: "bot says message",
-        process: function(bot,msg,suffix){ bot.sendMessage(msg.channel,suffix,true);}
-    },
-    "image": {
-        usage: "<image tags>",
-        description: "gets image matching tags from google",
-        process: function(bot,msg,suffix){ google_image_plugin.respond(suffix,msg.channel,bot);}
-    },
-    "pullanddeploy": {
-        description: "bot will perform a git pull master and restart with the new code",
-        process: function(bot,msg,suffix) {
-            bot.sendMessage(msg.channel,"fetching updates...",function(error,sentMsg){
-                console.log("updating...");
-	            var spawn = require('child_process').spawn;
-                var log = function(err,stdout,stderr){
-                    if(stdout){console.log(stdout);}
-                    if(stderr){console.log(stderr);}
-                };
-                var fetch = spawn('git', ['fetch']);
-                fetch.stdout.on('data',function(data){
-                    console.log(data.toString());
-                });
-                fetch.on("close",function(code){
-                    var reset = spawn('git', ['reset','--hard','origin/master']);
-                    reset.stdout.on('data',function(data){
-                        console.log(data.toString());
-                    });
-                    reset.on("close",function(code){
-                        var npm = spawn('npm', ['install']);
-                        npm.stdout.on('data',function(data){
-                            console.log(data.toString());
-                        });
-                        npm.on("close",function(code){
-                            console.log("goodbye");
-                            bot.sendMessage(msg.channel,"brb!",function(){
-                                bot.logout(function(){
-                                    process.exit();
-                                });
-                            });
-                        });
-                    });
-                });
-            });
-        }
-    },
-    "meme": {
-        usage: 'meme "top text" "bottom text"',
-        process: function(bot,msg,suffix) {
-            var tags = msg.content.split('"');
-            var memetype = tags[0].split(" ")[1];
-            //bot.sendMessage(msg.channel,tags);
-            var Imgflipper = require("imgflipper");
-            var imgflipper = new Imgflipper(AuthDetails.imgflip_username, AuthDetails.imgflip_password);
-            imgflipper.generateMeme(meme[memetype], tags[1]?tags[1]:"", tags[3]?tags[3]:"", function(err, image){
-                //console.log(arguments);
-                bot.sendMessage(msg.channel,image);
-            });
-        }
-    },
-    "memehelp": { //TODO: this should be handled by !help
-        description: "returns available memes for !meme",
-        process: function(bot,msg) {
-            var str = "Currently available memes:\n"
-            for (var m in meme){
-                str += m + "\n"
-            }
-            bot.sendMessage(msg.channel,str);
-        }
-    },
+    },	
     "log": {
         usage: "<log message>",
         description: "logs message to bot console",
         process: function(bot,msg,suffix){console.log(msg.content);}
-    },
-    "wiki": {
-        usage: "<search terms>",
-        description: "returns the summary of the first matching search result from Wikipedia",
-        process: function(bot,msg,suffix) {
-            var query = suffix;
-            if(!query) {
-                bot.sendMessage(msg.channel,"usage: !wiki search terms");
-                return;
-            }
-            var Wiki = require('wikijs');
-            new Wiki().search(query,1).then(function(data) {
-                new Wiki().page(data.results[0]).then(function(page) {
-                    page.summary().then(function(summary) {
-                        var sumText = summary.toString().split('\n');
-                        var continuation = function() {
-                            var paragraph = sumText.shift();
-                            if(paragraph){
-                                bot.sendMessage(msg.channel,paragraph,continuation);
-                            }
-                        };
-                        continuation();
-                    });
-                });
-            },function(err){
-                bot.sendMessage(msg.channel,err);
-            });
-        }
-    },
-    "join-server": {
-        usage: "<invite>",
-        description: "joins the server it's invited to",
-        process: function(bot,msg,suffix) {
-            console.log(bot.joinServer(suffix,function(error,server) {
-                console.log("callback: " + arguments);
-                if(error){
-                    bot.sendMessage(msg.channel,"failed to join: " + error);
-                } else {
-                    console.log("Joined server " + server);
-                    bot.sendMessage(msg.channel,"Successfully joined " + server);
-                }
-            }));
-        }
-    },
-    "stock": {
-        usage: "<stock to fetch>",
-        process: function(bot,msg,suffix) {
-            var yahooFinance = require('yahoo-finance');
-            yahooFinance.snapshot({
-              symbol: suffix,
-              fields: ['s', 'n', 'd1', 'l1', 'y', 'r'],
-            }, function (error, snapshot) {
-                if(error){
-                    bot.sendMessage(msg.channel,"couldn't get stock: " + error);
-                } else {
-                    //bot.sendMessage(msg.channel,JSON.stringify(snapshot));
-                    bot.sendMessage(msg.channel,snapshot.name
-                        + "\nprice: $" + snapshot.lastTradePriceOnly);
-                }  
-            });
-        }
-    },
-    "rss": {
-        description: "lists available rss feeds",
-        process: function(bot,msg,suffix) {
-            /*var args = suffix.split(" ");
-            var count = args.shift();
-            var url = args.join(" ");
-            rssfeed(bot,msg,url,count,full);*/
-            bot.sendMessage(msg.channel,"Available feeds:", function(){
-                for(var c in rssFeeds){
-                    bot.sendMessage(msg.channel,c + ": " + rssFeeds[c].url);
-                }
-            });
-        }
     },
     "reddit": {
         usage: "[subreddit]",
@@ -363,7 +354,7 @@ bot.on("message", function (msg) {
             suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+2);
         }
 		var cmd = commands[cmdTxt];
-        if(cmdTxt === "help"){
+        if(cmdTxt === "gammaagefive"){
             //help is special since it iterates over the other commands
             for(var cmd in commands) {
                 var info = "!" + cmd;
@@ -390,9 +381,6 @@ bot.on("message", function (msg) {
             return;
         }
         
-        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
-                bot.sendMessage(msg.channel,msg.author + ", you called?");
-        }
     }
 });
  
