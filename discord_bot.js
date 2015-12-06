6c71c6102d9ad824253f4cde55187c148dfa50b4
Remco <SteamingMutt@users.noreply.github.com>
2015-12-06 12:02:30 +0100

    Better version variable

--- discord_bot.js
+++ discord_bot.js
@@ -10,7 +10,7 @@ var VersionChecker	= require("./versioncheck");
 
 var maintenance;
 
-var version = "1.2.11";
+var version = require("./package.json").version;
 
 var Discord = require("discord.js");
 
