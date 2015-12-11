e1a3a9dcb5c58fa3fb8245dd0d6cd31665950010
Perpetucake <perpetucake@gmail.com>
2015-12-11 00:05:49 -0600

    Revolvo is patched CSGO
    Revolvo is patched CSGO

--- discord_bot.js
+++ discord_bot.js
@@ -643,6 +643,48 @@ var commands = {
         });
       }
     },
+    "rule34": {
+      description: "Rule#34 : If it exists there is porn of it. If not, start uploading.",
+      process: function(bot,msg,suffix) {
+        var request = require('request');
+        var xml2js = require('xml2js');
+        var url = "http://rule34.xxx/index.php?page=dapi&s=post&limit=1&q=index&tags="+suffix;
+        request(url, function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            //console.log(body)
+            xml2js.parseString(body, function (err, result) {
+              var util = require('util');
+              //console.log(result)
+              //console.log("1")
+              //console.log(util.inspect(result, false, null))
+              //console.log("2")
+              //console.log(util.inspect(result["posts"], false, null))
+              var fuckme = util.inspect(result.posts,false,null).split("'");
+              //console.log(fuckme[14])
+              bot.sendMessage(msg.channel,fuckme[13]);
+              //console.log("3")
+              //console.log(util.inspect(result["posts"["post"]], false, null))
+              //console.log("4")
+              //console.log(util.inspect(result["posts"["post"[1["'$'"]]]], false, null))
+
+              //result = JSON.parse(result)
+              //console.log(result)
+              //bot.sendMessage(msg.channel,result.file_url)
+                                        }
+                                   );
+          } else {
+            console.log("Got an error: ", error, ", status code: ", response.statusCode);
+          }
+        }
+      );
+      }
+    },
+    "navy":    {
+         description: "What are seals?",
+          process: function(bot,msg)    {
+            bot.sendMessage(msg, "What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.");
+        }
+    },
     "urbandictionary": {
       name: "urbandictionary",
       extendedhelp: "Every wanted to know what idiots on the internet thinks something means? Here ya go!",
