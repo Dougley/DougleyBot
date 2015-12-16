a64d663cca7f8bf8cbef5dd25c184140c9d07992
Remco <SteamingMutt@users.noreply.github.com>
2015-12-16 11:14:34 +0100

    Try 2 at fixing image

--- discord_bot.js
+++ discord_bot.js
@@ -408,10 +408,35 @@ var commands = {
     extendedhelp: "I'll search teh interwebz for a picture matching your tags.",
     usage: "<image tags>",
     process: function(bot, msg, suffix) {
-      google_image_plugin.respond(suffix, msg.channel, bot);
+      if(!ConfigFile || !ConfigFile.youtube_api_key || !ConfigFile.google_custom_search){
+      			bot.sendMessage(msg.channel, "Image search requires both a YouTube API key and a Google Custom Search key!");
+      			return;
+      		}
+      		//gets us a random result in first 5 pages
+      		var page = 1 + Math.floor(Math.random() * 5) * 10; //we request 10 items
+      		request("https://www.googleapis.com/customsearch/v1?key=" + ConfigFile.youtube_api_key + "&cx=" + ConfigFile.google_custom_search + "&q=" + (args.replace(/\s/g, '+')) + "&searchType=image&alt=json&num=10&start="+page, function(err, res, body) {
+      			var data, error;
+      			try {
+      				data = JSON.parse(body);
+      			} catch (error) {
+      				console.log(error)
+      				return;
+      			}
+      			if(!data){
+      				console.log(data);
+      				bot.sendMessage(msg.channel, "Error:\n" + JSON.stringify(data));
+      				return;
+      			}
+      			else if (!data.items || data.items.length == 0){
+      				console.log(data);
+      				bot.sendMessage(msg.channel, "No result for '" + args + "'");
+      				return;
+      			}
+      			var randResult = data.items[Math.floor(Math.random() * data.items.length)];
+      			bot.sendMessage(msg.channel, randResult.title + '\n' + randResult.link);
+      		});
       CmdErrorLog.log("debug", "I've looked for images of " + suffix + " for " + msg.sender.username);
-    }
-  },
+  }},
   "pullanddeploy": {
     name: "pullanddeploy",
     description: "Bot will perform a git pull master and restart with the new code.",
